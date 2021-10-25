e1d0b97c0b27652d523826e09d41ae4578523a97
dayanenubia <41790447+dayanenubia@users.noreply.github.com>
2021-10-25 19:38:02 -0300

    Add files via upload

---
+++ TiraTeima16.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int x, y;
+	printf("Entre com as coordenaas da bola: ");
+	scanf("%d%d", &x, &y);
+	if(x >= 0 && x <= 432 && y >= 0 && y <= 468){
+		printf("Dentro\n");
+	} else {
+		printf("Fora\n");
+	}
+	return 0;
+}
